50c8486d4b144aad72f997aee908e2467a72aeb8
Jimmy <jimmy.martel@epitech.eu>
2022-04-23 17:47:22 +0200

    82. Pages & Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Offers from "./pages/Offers";
+import Explore from "./pages/Explore";
+import ForgotPassword from "./pages/ForgotPassword";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
     return (
         <>
-            <h1>My App</h1>
+            <Router>
+                <Routes>
+                    <Route path={"/"} element={<Explore/>}/>
+                    <Route path={"/forgot-password"} element={<ForgotPassword/>}/>
+                    <Route path={"/offers"} element={<Offers/>}/>
+                    <Route path={"/profile"} element={<Profile/>}/>
+                    <Route path={"/sign-in"} element={<SignIn/>}/>
+                    <Route path={"/sign-up"} element={<SignUp/>}/>
+                </Routes>
+            </Router>
         </>
     );
 };
