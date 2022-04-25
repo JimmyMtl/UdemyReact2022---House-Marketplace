414f73600011c6a795ff4511ee88d94d8c0f93c2
Jimmy <jimmy.martel@epitech.eu>
2022-04-25 11:37:39 +0200

    91. PrivateRoute Component & useAuthStatus Hook

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Navbar from "./components/Navbar";
+import PrivateRoute from "./components/PrivateRoute";
 import {ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css'
+
 const App = () => {
     return (
         <>
@@ -16,7 +18,10 @@ const App = () => {
                     <Route path={"/"} element={<Explore/>}/>
                     <Route path={"/forgot-password"} element={<ForgotPassword/>}/>
                     <Route path={"/offers"} element={<Offers/>}/>
-                    <Route path={"/profile"} element={<Profile/>}/>
+
+                    <Route path={"/profile"} element={<PrivateRoute/>}>
+                        <Route path={"/profile"} element={<Profile/>}/>
+                    </Route>
                     <Route path={"/sign-in"} element={<SignIn/>}/>
                     <Route path={"/sign-up"} element={<SignUp/>}/>
                 </Routes>
---
+++ src/temp.js
@@ -0,0 +1,28 @@
+fetch("https://v2-api.seekube.com/events/62022fba46a08c2f00297c26/exponents/6253fe01e848706d25da0536", {
+"headers": {
+"accept": "*/*",
+"accept-language": "fr-FR,fr;q=0.9",
+"authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWFlMGE3ZTdlNWVlNDZmNjY4NDRmOTYiLCJ1c2VybmFtZSI6InRAdC5mciIsImlhdCI6MTY1MDg3NzQ3M30.PH6gesX2Fp3UpyC31vNNYjfotrZDzUmwTCnOUvK0aIE",
+"cache-control": "no-cache",
+"content-type": "application/json",
+"koaupdatecache": "/events/62022fba46a08c2f00297c26/exponents/byOrganization/6253fdff27427575aad0598b",
+"pragma": "no-cache",
+"sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"99\", \"Opera GX\";v=\"85\"",
+"sec-ch-ua-mobile": "?1",
+"sec-ch-ua-platform": "\"Android\"",
+"sec-fetch-dest": "empty",
+"sec-fetch-mode": "cors",
+"sec-fetch-site": "same-site",
+"x-datadog-origin": "rum",
+"x-datadog-parent-id": "2934619771418723443",
+"x-datadog-sampled": "1",
+"x-datadog-sampling-priority": "1",
+"x-datadog-trace-id": "2050357169884292948"
+},
+"referrer": "https://app.seekube.com/",
+"referrerPolicy": "strict-origin-when-cross-origin",
+"body": "{\"multipleUsers\":[{\"_user\":{\"username\":\"t@t.fr\",\"fullName\":\"t@t.fr\",\"title\":\"\",\"firstName\":\"\",\"lastName\":\"\"},\"role\":\"manager\"}]}",
+"method": "PATCH",
+"mode": "cors",
+"credentials": "include"
+});
