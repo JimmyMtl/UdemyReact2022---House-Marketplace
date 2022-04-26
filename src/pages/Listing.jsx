b02cef611ff29382925feb4f1814760b7425c5fb
Jimmy <jimmy.martel@epitech.eu>
2022-04-26 15:04:55 +0200

    Listing Details

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -35,9 +35,72 @@ const Listing = () => {
     return <Spinner />;
   }
   return (
-    <div>
-      {params.categoryName} {params.listingId}
-    </div>
+    <main>
+      {/* SLIDER */}
+      <div
+        className="shareIconDiv"
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
+        }}
+      >
+        <img src={shareIcon} alt="Share" />
+      </div>
+
+      {shareLinkCopied && <p className={"linkCopied"}>Copied</p>}
+
+      <div className="linstingDetails">
+        <p className="listingName">
+          {listing.name} - €
+          {listing.offer
+            ? listing.discountedPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+            : listing.regularPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+        </p>
+        <p className="listingLocation">{listing.location}</p>
+        <p className="listingType">
+          For {listing.type === "rent" ? "Rent" : "Sale"}
+        </p>
+        {listing.offer && (
+          <p className="discountPrice">
+            €
+            {(listing.regularPrice - listing.discountedPrice)
+              .toString()
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}
+            discount
+          </p>
+        )}
+        <ul className="listingDetailsList">
+          <li>
+            {listing.bedrooms > 1
+              ? `${listing.bedrooms} Bedrooms`
+              : "1 Bedroom"}
+          </li>
+          <li>
+            {listing.bathrooms > 1
+              ? `${listing.bathrooms} Bathrooms`
+              : "1 Bathroom"}
+          </li>
+          <li>{listing.parking && "Parking Spot"}</li>
+          <li>{listing.furnished && "Furnished"}</li>
+        </ul>
+        <p className="listingLocationTitle">Location</p>
+        {auth.currentUser?.uid !== listing.userRef && (
+          <Link
+            to={`/contact/${listing.userRef}?listingName=${listing.name}&listingLocation=${listing.location}`}
+            className={"primaryButton"}
+          >
+            Contact Landlord
+          </Link>
+        )}
+      </div>
+    </main>
   );
 };
 
